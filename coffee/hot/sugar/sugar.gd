4fbb8d00722a60e59df6626f731226eeed7dc331
FMABr <felipemuamorim@gmail.com>
2025-01-20 20:26:12 -0400

    0.1.1.2 Hot Coffee - placeholder sugar scene

---
+++ coffee/hot/sugar/sugar.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var sugar_counter_label: Label = %SugarCounterLabel
+
+var sugar_spoons: int = 0:
+	set(value):
+		sugar_spoons = value
+		_update_label(value)
+
+
+func _on_add_sugar_button_pressed() -> void:
+	sugar_spoons += 1
+
+
+func _update_label(new_value: int) -> void:
+		sugar_counter_label.text = "Colheres de açucar: %s" % new_value
+	
