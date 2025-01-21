ab91e7fa17cb84aee3a96956b508442daea078b2
FMABr <felipemuamorim@gmail.com>
2025-01-20 20:37:50 -0400

    0.1.1.3 Hot Coffee - placeholder milk screen

---
+++ coffee/hot/milk/milk.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_small_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_medium_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_large_pressed() -> void:
+	pass # Replace with function body.
