c88738aa712cf49e90d04217b1bf13d9d6478023
Asunder32 <erikviana32@gmail.com>
2025-01-24 19:20:56 -0400

    0.1.2.2 - Imports, gerador de gosto e jsonHandler como global

--- taste/taste_generator.gd
+++ taste/taste_generator.gd
@@ -1,11 +1,31 @@
 extends Node
 
+var want_it_cold := false
+var want_a_lot_of_sugar := false
+var want_a_lot_of_milk := false
+var want_a_lot_of_foam := false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var rng := RandomNumberGenerator.new()
 
+func generate_taste() -> Array:
+	var selector_number := rng.randi_range(0, 15)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if selector_number >= 8:
+		want_a_lot_of_foam = true
+		selector_number -= 8
+	
+	if selector_number >= 4:
+		want_a_lot_of_milk = true
+		selector_number -= 4
+		
+	if selector_number >= 2:
+		want_a_lot_of_sugar = true
+		selector_number -= 2
+		
+	if selector_number >= 1:
+		want_it_cold = true
+		selector_number -= 1
+
+	var taste_list := [want_it_cold, want_a_lot_of_sugar, want_a_lot_of_milk, want_a_lot_of_foam]
+	return taste_list
+	
