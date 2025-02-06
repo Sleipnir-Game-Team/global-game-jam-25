584a75e3ee806f220a2f31f5c30ef69bb7bb9dbd
Erik Felipe Viana Rocha <erikviana32@gmail.com>
2025-02-06 19:39:50 -0400

    0.1.5.1 - add the ice and sugar assets on the mini-games

--- coffee/cold/1_ice/ice.gd
+++ coffee/cold/1_ice/ice.gd
@@ -1,20 +1,32 @@
 extends CoffeeStep
 
 @onready var ice_counter_label: Label = %IceCounterLabel
+@onready var ice_cube_animation: AnimationPlayer = %ice_animation
+@onready var ice_cube_sprite: Sprite2D = %Ice_cube_sprite
+
+var can_be_pressed := true
 
 var ice_cubes: int = 0:
 	set(value):
 		ice_cubes = value
 		_update_label(value)
-
+		
+func _ready() -> void:
+	ice_cube_sprite.visible = false
 
 func _update_label(new_value: int) -> void:
 		ice_counter_label.text = "Cubos de gelo: %s" % new_value
 
-
 func _on_add_ice_button_pressed() -> void:
-	ice_cubes += 1
+	if can_be_pressed:
+		can_be_pressed = false
+		ice_cube_animation.play('ice_cube_animation')
+		ice_cubes += 1
 
 
 func _on_next_pressed() -> void:
 	end.emit(ice_cubes)
+
+
+func _on_ice_animation_animation_finished(anim_name: StringName) -> void:
+	can_be_pressed = true
--- coffee/hot/1_sugar/sugar.gd
+++ coffee/hot/1_sugar/sugar.gd
@@ -1,14 +1,20 @@
 extends CoffeeStep
 
 @onready var sugar_counter_label: Label = %SugarCounterLabel
+@onready var sugar_jar_sprite: Button = %AddSugarButton
+@onready var sugar_jar_animation: AnimationPlayer = %sugar_animation
 
 
 var sugar_spoons: int = 0:
 	set(value):
 		sugar_spoons = value
 		_update_label(value)
+		
+func _ready() -> void:
+	sugar_jar_sprite.set_button_icon(load('res://assets/Assets finalizados/Acucar A.png'))
 
 func _on_add_sugar_button_pressed() -> void:
+	sugar_jar_animation.play('sugar_animation')
 	sugar_spoons += 1
 
 
